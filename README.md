3b6607990b458c86cd9930e8553d1fa8d8e196a9
Kristopher Doidge <Kristopher.Doidge@agilethought.com>
2017-09-22 11:15:26 -0400

    Readme has server info . C# file is a simple example connection to Database and runs a query.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# JITAarmory
+Server name : armory.database.windows.net
+Server Strings: Server=tcp:armory.database.windows.net,1433;Initial Catalog=destinyArmory;Persist Security Info=False;User ID={jita};Password={password1!};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+
+username = jita
+password = password1!
+
+Useful resources : 
+	Getting connected to Database via .NET
+		https://docs.microsoft.com/en-us/azure/sql-database/sql-database-connect-query-dotnet-visual-studio
---
+++ sqltest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try 
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "armory.database.windows.net";
+                builder.UserID = "jita";
+                builder.Password = "password1!";
+                builder.InitialCatalog = "destinyArmory";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();       
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT * FROM [dbo].[num]");
+                    String sql = sb.ToString();
+                    Console.WriteLine(sql);
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        Console.WriteLine("=========================================\n");
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            Console.WriteLine("=========================================\n");
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("=========================================\n");
+                                Console.WriteLine("{0}", reader.GetInt32(0));
+                            }
+                        }
+                    }                    
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        
+
+        }
+    }
+}
